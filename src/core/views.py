8c335fcfbe135e4ff5c51d92bd695c38003c435a
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2012-01-23 19:55:42 -0200

    Removing src folder

--- src/core/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from jsonrpc import jsonrpc_method
-from core.models import Device, DeviceLocation
-
-@jsonrpc_method('FindMyDevice.add_location')
-def add_location(request, username, device_id, lat, lon):
-    return {'status': 'success'}
