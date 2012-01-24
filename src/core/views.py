e0b34dc4dfd702fea4bb8676e6fd542464d1a429
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2012-01-23 22:37:55 -0200

    Layout changes

--- src/core/views.py
+++ src/core/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render_to_response
 def homepage(request, template=None):
     context = RequestContext(request)
     return render_to_response(template, context)
+
+
+def profile(request, template=None):
+    context = RequestContext(request)
+    return render_to_response(template, context)
--- src/urls.py
+++ src/urls.py
@@ -5,9 +5,17 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Admin
     url(r'^admin/', include(admin.site.urls)),
+    # Home
     (r'^$', 'core.views.homepage', {'template': 'index.html'}),
+    # User profile
+    url(r'^accounts/profile/$', 'core.views.profile', {'template': 'profile.html'}),
+    # Logout using django.contrib.auth
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    # Django-registration
     (r'^accounts/', include('registration.urls')),
+    # RESTful APIs
     (r'^api/', include('api.urls')),
 )
 
