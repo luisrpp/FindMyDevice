3461dcc00cc6ca43448315cd92e4091647858eb6
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2012-01-27 11:21:37 -0200

    Sidebar context processor

--- src/context_processors.py
+++ src/context_processors.py
@@ -3,6 +3,7 @@ from core.models import Device
 
 
 def sidebar_devices(request):
+    """Sidebar context processor"""
     if request.user.id != None:
         devices = Device.objects.filter(user=request.user)
         return {'devices': devices}
